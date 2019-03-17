25954f7c4a40e0be46de3936d7829ac4c014c6c2
Bloectasy <47886919+Bloectasy@users.noreply.github.com>
2019-03-17 21:50:05 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,14 +8,11 @@ module.exports.run = async (bot , message, args) => {
 
     .setColor('RANDOM')
     .addField("This bot is created by Bloectasy#6969.", "And he is cool.", true/false )
-    .addField("This bot currently has 6 commands which are the following:", "Afk,Report,Level,Ban and Kick", true/false)
+    .addField("This bot currently has 2 commands which are the following:", "Afk and Report", true/false)
     .addField("Afk: Which sets you as afk so people know that you are away when they mention you.","Usage of Afk: $afk [reason]", true/false)
     .addField("Report: This is a command if you want to report someone and one of the online staff member will get notified and will get to you as fast as possible.","Usage of Report: $report [@person] [reason]", true/false)
     .addField("Level: Which shows what level are you.","Usage of Level: $level", true/false)
-    .addField("The following commands are only for staff members:")
-    .addField("Ban: Is simply to ban a member.","Usage of Ban: $ban [@person]", true/false)
-    .addField("Kick: Is simply to kick a member they can join back if they have another invite.","Usage of Kick: $kick [@person]", true/false)
-    .setFooter("Staff commands are a lil bit unstable.I hope you enjoy the bot commands there will be more comming soon!")
+    .setFooter("Hope you enjoy the bot")
 
     try{
         message.author.send(help)
